92f6d9d44bb63ad995af17ae46f85d94c9b62879
Donnie Walsh <donfather1000@gmail.com>
2021-09-16 15:48:24 +0000

    revisited countOnly to create my own solution

--- countOnly.js
+++ countOnly.js
@@ -1,52 +1,13 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`🛳 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`⛔  Assertion failed: ${actual} != ${expected}`);
-  }
-};
-
-const countOnly = function (allItems, itemsToCount) {
-  const results = [];
-
-  for (const item of allItems) {
-    if (itemsToCount[item]) {
-      if (results[item]) {
-        results[item] += 1;
+const countOnly = function (arrayInput, objectKey) {
+  let returnObject = {};
+  for (index of arrayInput) {
+    if (objectKey[index]) {
+      if (returnObject[index]) {
+        returnObject[index] += 1;
       } else {
-        results[item] = 1;
+        returnObject[index] = 1;
       }
-      console.log(item);
     }
   }
-
-  return results;
-
-
+  return returnObject;
 }
-
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
-];
-
-const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
-
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined);
-
-//given array and object
-
-
-//returns object containing count of everything that 
-//the input object listed
-
