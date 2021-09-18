f339575dfdb2d469e663e24a2e3e514ac1048a3a
Donnie Walsh <donfather1000@gmail.com>
2021-09-18 22:21:59 +0000

    needed to refactor the eqArrays function for more appropriate use in the eqObjects function and updated the library to reflect the changes

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,10 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`🛳 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`⛔  Assertion failed: ${actual} != ${expected}`);
-  }
-};
-
 const eqArrays = function (a, b) {
+  if (!Array.isArray(a) || !Array.isArray(b)) {
+    return false;
+  }
+  if (a.length !== b.length) {
+    return false;
+  }
   for (let i = 0; i < a.length; i++) {
     if (a[i] !== b[i]) {
       return false;
@@ -14,10 +12,3 @@ const eqArrays = function (a, b) {
   }
   return true;
 }
-
-
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 4], [1, 4, 3]), true);
-assertEqual(eqArrays([1, -5, 3], [1, 252525, 3]), true);
-assertEqual(eqArrays([1, 'dsfg', 3], [1, 2, 3]), true);
