ede9e2fbeddf1c54d91a8576c17490a54aa1c2ab
Donnie Walsh <donfather1000@gmail.com>
2021-09-18 02:25:28 +0000

    defined the function to return the key of an object based on the value that is passed into the function. returns undefined if there is no match.

---
+++ findKeyByValue.js
@@ -0,0 +1,10 @@
+const findByKeyValue = function (object, keyValue) {
+  const values = Object.values(object);
+  for (let i = 0; i < values.length; i++) {
+    if (values[i] === keyValue) {
+      return Object.keys(object)[i];
+    }
+  }
+
+  return undefined;
+}
