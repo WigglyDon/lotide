f38399814c7790066e7984db980691a3fff52870
Donnie Walsh <donfather1000@gmail.com>
2021-09-18 08:57:15 +0000

    can't quite get the deep comparison to work on eqObjects. Will revisit later.

--- eqObjects.js
+++ eqObjects.js
@@ -1,10 +1,3 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`🛳 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`⛔  Assertion failed: ${actual} != ${expected}`);
-  }
-};
 const eqArrays = function (a, b) {
   for (let i = 0; i < a.length; i++) {
     if (a[i] !== b[i]) {
@@ -14,18 +7,41 @@ const eqArrays = function (a, b) {
   return true;
 }
 
-
 const eqObjects = function (object1, object2) {
 
-  //converts keys and values into an array
-  if (eqArrays(Object.keys(object1), Object.keys(object2))) {
-    if (eqArrays(Object.values(object1), Object.values(object2))) {
-      return true;
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+  for (let key of keys1) {
+    if (Array.isArray(key)) {
+      if (object1[key].length === object2[key].length) {
+        if (!eqArrays(object1[key], object2[key])) {
+          return false;
+        }
+      }
+    }
+    else {
+      return (object1[key] === object2[key]);
     }
   }
-  return false;
-
+  return true;
 }
+
+
+
+
+// **** FIX ME
+// const cd = { c: "1", d: ["23", 3] };
+// const dc = { d: ["2", 3], c: "1" };
+// console.log(eqObjects(cd, dc));
+
+// const cd2 = { c: "1", d: ["2", 3, 4] };
+// console.log(eqObjects(cd, cd2));
+
+
+
 
 
 const ab = { a: "1", b: "2" };
@@ -35,9 +51,6 @@ console.log(eqObjects(ab, ba)); // => true
 const abc = { a: "1", b: "2", c: "3" };
 console.log(eqObjects(ab, abc)); // => false
 
-// const cd = { c: '1', d: ['2', 3] };
-// const dc = { d: ['2', 3], c: '1' };
-// console.log(eqObjects(cd, dc)); //should be true
 
-// const cd2 = { c: '1', d: ['2', 3, 4] };
-// eqObjects(cd, cd2); //should be false;
+
+
--- findKey.js
+++ findKey.js
@@ -1,16 +1,23 @@
 const findKey = function (object, callback) {
+  const objectKeys = Object.keys(object);
+  for (let i = 0; i < objectKeys.length; i++) {
+    if (callback(object)) {
+      return object[i];
+    }
+  }
 
-return undefined;
+  return undefined;
 }
 
+result1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 2) // => "noma"
 
-const testObject = {
-1: 'a',
-2: 'b',
-3: 'c'
-}
-
-result1 = findKey(testObject, (x) => {return true});
 console.log(result1);
 
 
@@ -18,3 +25,36 @@ console.log(result1);
 // It should scan the object 
 // and return the first key for which the callback returns a truthy value. 
 // If no key is found, then it should return undefined.
+
+
+
+// //----------------------------------------------------------------------
+// const findKeyByValue = function (object, value) {
+//   let objectValues = Object.values(object);
+//   for (let i = 0; i < objectValues.length; i++) {
+//     if (objectValues[i] === value) {
+//       return Object.keys(object)[i];
+//     }
+//   }
+//  return undefined;
+// }
+
+
+// const testObject = {
+//  "a": 1,
+//  "b": 2,
+//  "c": 3
+// }
+
+// console.log(findKeyByValue(testObject, 1));
+// console.log(findKeyByValue(testObject, 2));
+// console.log(findKeyByValue(testObject, 3));
+// console.log(findKeyByValue(testObject, 4));
+
+
+
+
+// // It should scan the object and return the first key which contains the given value. 
+// // If no key with that given value is found, then it should return undefined.
+
+
