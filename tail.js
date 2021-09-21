eb591bfca8a03d02c5b555327445016de0113636
Donnie Walsh <donfather1000@gmail.com>
2021-09-21 03:06:04 +0000

    added an index file which allows the calling of any function using a prefix (prefix unimplemented, I think it's just 'index' but i'm still learning xD

--- tail.js
+++ tail.js
@@ -1,4 +1,7 @@
 const tail = function(array) {
+  if (array.length === 1) {
+    return array;
+  }
   return array.slice(1);
 };
 
--- test/tail.test.js
+++ test/tail.test.js
@@ -1,5 +1,15 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-assertArraysEqual(tail([5, 6, 7]), [6, 7]);
-assertArraysEqual(tail(["Hello", "Lighthouse", "Labs"]), "Hello");
+describe("#tail", () => {
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.deepEqual(tail(['5']), ['5']); 
+  });
+
+  it("returns [1, 2] for for [1, 2]", () => {
+    assert.deepEqual([1, 2], [1, 2]);
+  });
+});
