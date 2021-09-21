fc6219952fa0460fea70a3651b6f6445c1d0d2a1
Donnie Walsh <donfather1000@gmail.com>
2021-09-21 02:22:51 +0000

    refactored middle a bit and added a test file

--- middle.js
+++ middle.js
@@ -1,21 +1,16 @@
 const middle = function (array) {
   let middle = [];
-  let middleOdd = (array[(array.length / 2) - 0.5]);
   if (array.length > 2) {
-    for (let i = 0; i < array.length; i++) {
-      //even total push statement
-      if (array.length / array[i] === 2) {
-        middle.push(array[i]);
-        middle.push(array[i + 1]);
-      }
-      //odd total push statement
-      if (array.length % 2 !== 0) {
-        return middleOdd;
-      
-      }
-
+    if (array.length % 2 !== 0) {
+      middle.push(array[Math.floor(array.length / 2)]);
+    }
+    if (array.length % 2 === 0) {
+      middle.push(array.length / 2);
+      middle.push(array.length / 2 + 1);
     }
   }
   return middle;
 }
+
+module.exports = middle;
 
